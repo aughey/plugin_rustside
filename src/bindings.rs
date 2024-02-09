40c6ebf4099e79b17e69290bce9984ec4d577621
John Aughey <jha@aughey.com>
2024-02-09 02:46:06 +0000

    asdf

--- src/bindings.rs
+++ src/bindings.rs
@@ -2,4 +2,9 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+//include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct plugin_CBoeingPackageModel {
+    _unused: [u8; 0],
+}
